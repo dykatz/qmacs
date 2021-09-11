7166e351e2d05656a42ede7e5a6bea5ac34cf416
Dylan Katz <dykatz@uw.edu>
2021-09-11 11:17:26 -0700

    Add text filter to buffer picker

--- buffer-picker.cc
+++ buffer-picker.cc
@@ -1,3 +1,5 @@
+#include <QLineEdit>
+#include <QSortFilterProxyModel>
 #include <QTableView>
 #include <QVBoxLayout>
 
@@ -7,12 +9,19 @@ BufferPicker::BufferPicker(BufferModel* model, QWidget* parent)
     : QDialog(parent, Qt::WindowFlags())
     , m_buffer_model(model)
 {
+    auto filter_line_edit = new QLineEdit(this);
+    auto buffer_model_filter_model = new QSortFilterProxyModel(this);
+    buffer_model_filter_model->setSourceModel(model);
+    connect(filter_line_edit, &QLineEdit::textChanged, buffer_model_filter_model, &QSortFilterProxyModel::setFilterWildcard);
+
     auto buffer_model_view = new QTableView(this);
-    buffer_model_view->setModel(m_buffer_model);
+    buffer_model_view->setModel(buffer_model_filter_model);
     buffer_model_view->setSelectionMode(QAbstractItemView::SingleSelection);
     buffer_model_view->setSelectionBehavior(QAbstractItemView::SelectRows);
+    buffer_model_view->selectRow(0);
 
     auto layout = new QVBoxLayout;
+    layout->addWidget(filter_line_edit);
     layout->addWidget(buffer_model_view);
     setLayout(layout);
 

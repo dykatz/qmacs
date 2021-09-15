443e842d9f506faef6e5b837f1fe5c5ecc1aca62
Dylan Katz <dykatz@uw.edu>
2021-09-14 21:59:52 -0700

    Make the font monospace

--- main.cc
+++ main.cc
@@ -9,6 +9,12 @@ int main(int argc, char** argv)
 	QCoreApplication::setOrganizationName("Qmacs");
 	QCoreApplication::setApplicationName("Qmacs");
 
+	QFont font;
+	font.setFamily("courier");
+	font.setStyleHint(QFont::Monospace);
+	font.setFixedPitch(true);
+	QGuiApplication::setFont(font);
+
 	Window window;
 	window.show();
 
